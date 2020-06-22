7109acf1215ff72ac96e9c91688e37da5689256d
Abtahee Ali <abtaheeali@gmail.com>
2020-06-22 16:13:20 -0400

    Added cats #147-#145

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ class App extends Component {
             <Cats
               octCatLink="https://octodex.github.com//terracottocat/"
               octCatImg="https://octodex.github.com/images/Terracottocat_Single.png"
+              octCatName="Terracottocat"
               octNumber="149:"
               octCatTitle="Terracottocat"
               octCatLink="https://octodex.github.com//terracottocat/"
@@ -23,12 +24,46 @@ class App extends Component {
             <Cats
               octCatLink="https://octodex.github.com/octogatos/"
               octCatImg="https://octodex.github.com/images/Octogatos.png"
+              octCatName="Octogatos"
               octNumber="148:"
               octCatTitle="Octogatos"
               octCatLink="https://octodex.github.com/octogatos/"
               authorLink="https://github.com/cameronfoxly"
               authorImg="https://avatars1.githubusercontent.com/u/19292210?s=400&u=80cd8a15836c3fc23c2efc60c43299e34aa77b3e&v=4"
               altAuthorName="User: CameronFoxly"
+            />
+            <Cats
+              octCatLink="https://octodex.github.com/adacats/"
+              octCatImg="https://octodex.github.com/images/Adacats.png"
+              octCatName="Adacats"
+              octNumber="147:"
+              octCatTitle="Adacats"
+              octCatLink="https://octodex.github.com/adacats/"
+              authorLink="https://github.com/cameronfoxly"
+              authorImg="https://avatars1.githubusercontent.com/u/19292210?s=400&u=80cd8a15836c3fc23c2efc60c43299e34aa77b3e&v=4"
+              altAuthorName="User: CameronFoxly"
+            />
+            <Cats
+              octCatLink="https://octodex.github.com/fintechtocat/"
+              octCatImg="https://octodex.github.com/images/Fintechtocat.png"
+              octCatName="Fintechtocat"
+              octNumber="146:"
+              octCatTitle="Fintechtocat"
+              octCatLink="https://octodex.github.com/fintechtocat/"
+              authorLink="https://github.com/ceciliorz"
+              authorImg="https://avatars0.githubusercontent.com/u/29592817?s=400&u=fd364426e0ae59260c08d37e76979b9e458b913b&v=4"
+              altAuthorName="User: Ceciliorz"
+            />
+            <Cats
+              octCatLink="https://octodex.github.com/brennatocat/"
+              octCatImg="https://octodex.github.com/images/Brennatocat.png"
+              octCatName="Brennatocat"
+              octNumber="145:"
+              octCatTitle="Brennatocat"
+              octCatLink="https://octodex.github.com/brennatocat/"
+              authorLink="https://github.com/ceciliorz"
+              authorImg="https://avatars0.githubusercontent.com/u/29592817?s=400&u=fd364426e0ae59260c08d37e76979b9e458b913b&v=4"
+              altAuthorName="User: Ceciliorz"
             />
           </section>
         </main>
